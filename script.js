6e5c428e27c21bf1378862dc0e6ef2d9fbbceb02
sauxsolution-web <sauxsolution@gmail.com>
2026-05-22 19:08:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+window.addEventListener("scroll", function () {
 
+const header = document.querySelector("header");
+
+if (window.scrollY > 50) {
+    header.style.background = "#000814";
+} else {
+    header.style.background = "rgba(0,0,0,0.7)";
+}
+
+});
+
+console.log("SAUX Website Loaded");
