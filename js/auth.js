a6b4776235d4759d7ad84a7b9df2d65e6af4e43d
sauxsolution-web <sauxsolution@gmail.com>
2026-05-25 21:56:37 +0530

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,42 @@
+const encodedUser = "c2F1eGFkbWlu";
+const encodedPass = "c2F1eEAyMDI2";
+
+function decode(value){
+
+return atob(value);
+
+}
+
+function login(){
+
+const username =
+document.getElementById("username").value;
+
+const password =
+document.getElementById("password").value;
+
+if(
+
+username === decode(encodedUser) &&
+
+password === decode(encodedPass)
+
+){
+
+sessionStorage.setItem(
+"adminLoggedIn",
+"true"
+);
+
+window.location.href = "admin.html";
+
+}
+else{
+
+document.getElementById("error")
+.innerHTML =
+"Invalid Credentials";
+
+}
+
+}
