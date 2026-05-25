2aa24d3b0f51082f80d0a7c1f997a831e59ede22
sauxsolution-web <sauxsolution@gmail.com>
2026-05-25 22:52:10 +0530

    Update enquiry.js

--- js/enquiry.js
+++ js/enquiry.js
@@ -1,24 +1,48 @@
-document.getElementById("enquiryForm").addEventListener("submit", function(e){
+document
+.getElementById("enquiryForm")
+.addEventListener("submit", function(e){
 
 e.preventDefault();
 
 let enquiry = {
 
-name: document.getElementById("name").value,
-email: document.getElementById("email").value,
-phone: document.getElementById("phone").value,
-message: document.getElementById("message").value
+name:
+document.getElementById("name").value,
+
+email:
+document.getElementById("email").value,
+
+phone:
+document.getElementById("phone").value,
+
+message:
+document.getElementById("message").value,
+
+date:
+new Date().toLocaleString(),
+
+status:"New",
+
+executive:""
 
 };
 
-let enquiries = JSON.parse(localStorage.getItem("enquiries")) || [];
+let enquiries =
+JSON.parse(
+localStorage.getItem("enquiries")
+) || [];
 
 enquiries.push(enquiry);
 
-localStorage.setItem("enquiries", JSON.stringify(enquiries));
+localStorage.setItem(
+"enquiries",
+JSON.stringify(enquiries)
+);
 
 alert("Enquiry Submitted Successfully");
 
-document.getElementById("enquiryForm").reset();
+document
+.getElementById("enquiryForm")
+.reset();
 
 });
